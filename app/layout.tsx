f1a7bd4f9dcd363f3105b6ec1a1dd0af0c11320f
Drew <drewsepeczi@gmail.com>
2025-09-04 08:32:42 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "engage-cook",
+  description: "built using next.js, magic-ui, and tailwindcss",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="e2279467-b5f6-4e9d-8b62-869876b433f9"
+        ></script>
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
